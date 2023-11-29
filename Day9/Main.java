3a196058324a11e3804f45134740463cac1a7a82
Yakimax <sv1099912@gmail.com>
2023-11-29 20:54:27 +0530

    add

---
+++ Day9/Main.java
@@ -0,0 +1,25 @@
+import java.util.* ;
+class Main{
+
+    public void reverse ( String str ) {
+       StringBuilder sb = new StringBuilder() ;
+       int i  = 0 ; 
+       int j = str.length() ;
+       while(i<j){
+        sb.insert(0,str.charAt(i));
+        i++ ;
+       }
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner (System.in);
+        String str = scn.nextLine() ;
+        int i = 0 ;
+        StringBuilder sb = new StringBuilder() ;
+        while ( i < str.length() ) { 
+            while(){
+                
+            } 
+            sb.insert(i, );
+        }
+    }
+}
