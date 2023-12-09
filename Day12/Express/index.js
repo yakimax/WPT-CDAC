4442e11e4233825d2f2b340919d9e3be405d307c
Yakimax <sv1099912@gmail.com>
2023-12-09 22:05:05 +0530

    add

--- Day12/Express/index.js
+++ Day12/Express/index.js
@@ -11,7 +11,7 @@ app.get('/login',(request,response) => {
     response.send("Hi there") ;
 }) ;
 
-app.post('/sum/:a/:b',(request,response) => {
+app.post('/sum/a/b',(request,response) => {
     var x = request.params.a ;
     var y = request.params.b ;
     var z = x + y ;
@@ -19,6 +19,14 @@ app.post('/sum/:a/:b',(request,response) => {
     // console.log(z) ;
 }) ;
 
+app.post('/add',(request,response) => {
+    var data =  request.body ;
+    // response.send({sum : z}) ;
+    console.log(data) ;
+    response.send({message:"welcome"}) ;
+}) ;
+
+
 app.listen( 5490 , ( ) => {
     console.log ( "server started" ) ;
-});
+}) ;
